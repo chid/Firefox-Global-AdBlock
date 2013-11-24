a7ca204c1eb2a8a144ae0456e0970bc8a45a87b1
Charley Peng <cpeng92@gmail.com>
2013-11-24 13:30:16 +1100

    fixing issues

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -4,10 +4,10 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-mkdir /usr/lib/firefox-addons/extensions/{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}
-cd /usr/lib/firefox-addons/extensions/"{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}"
+mkdir /usr/share/mozilla/extensions/{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}
+cd /usr/share/mozilla/extensions/"{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}"
 wget --no-check-certificate https://addons.mozilla.org/downloads/latest/1865/adblock_plus-1.1.xpi
-unzip adblock_plus-1.1-fx+sm+tb.xpi
+unzip adblock_plus-1.1.xpi
 
 echo "Installed"
 
